7f883782ad723134f48e7cd7c8e80e9b956ff1a7
Alex Turner <alexlt@umich.edu>
2017-02-20 13:37:16 -0500

    Polygon for visible areas with monaco style lighting done.

---
+++ Assets/Scripts/Lighting/DemoMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoMove : MonoBehaviour {
+
+	public float moveTime = 2.0f;
+	public float moveAmount = 2.0f;
+
+	private Vector3 start;
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+		start = this.transform.position;
+		time = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+
+		if (time > moveTime * 2.0f)
+			time = 0.0f;
+
+		float amount = moveAmount * (time / moveTime);
+
+		if (time > moveTime)
+			amount = moveAmount * (1 - (time - moveTime) / moveTime);
+
+		this.transform.position = new Vector3 (start.x, start.y + amount, 0.0f);
+	}
+}
---
+++ Assets/Scripts/Lighting/LayerMonoBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerMonoBehavior : MonoBehaviour {
+	private static IDictionary<int, IList<GameObject>> layersCache = new Dictionary<int, IList<GameObject>> ();
+
+	public static IList<GameObject> FindGameObjectsWithLayer(int layer)
+	{
+		IList<GameObject> cache;
+		if (!layersCache.TryGetValue(layer, out cache)) // Might be a bitwise mask
+		{
+			cache = new List<GameObject>();
+
+			foreach (KeyValuePair<int, IList<GameObject>> pair in layersCache) {
+				if ((pair.Key & layer) != 0)
+				{
+					foreach (GameObject go in pair.Value)
+						cache.Add (go);
+				}
+			}
+
+			return cache;
+		}
+
+		return layersCache [layer];
+	}
+
+	public static GameObject FindWithLayer(int layer)
+	{
+		var cache = layersCache[layer];
+		if (cache != null && cache.Count > 0)
+			return cache[0];
+		else
+			return null;
+	}
+
+	protected void Awake() {
+		IList<GameObject> cache;
+		if (!layersCache.TryGetValue(gameObject.layer, out cache))
+		{
+			cache = new List<GameObject>();
+			layersCache.Add(gameObject.layer, cache);
+		}
+
+		cache.Add(gameObject);
+	}
+
+	protected void OnDestroy()
+	{
+		layersCache[gameObject.layer].Remove(gameObject);
+	}
+}
