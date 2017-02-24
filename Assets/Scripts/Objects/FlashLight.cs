ce2067787df4bf5a98206c1497a9aae85b40adca
Alex Turner <alexlt@umich.edu>
2017-02-24 14:15:22 -0500

    Flash Light Object

---
+++ Assets/Scripts/Objects/FlashLight.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour {
+
+	public Sprite flashLightOn;
+	public Sprite flashLightOff;
+
+	public GameObject fieldOfViewLight;
+
+	private SpriteRenderer sprend;
+
+
+	[SerializeField]
+	private bool _isOn = true;
+	public bool isOn {
+		set {
+			_isOn = value;
+
+			SwapSprites ();
+		}
+		get {
+			return _isOn;
+		}
+	}
+
+	void Start() {
+		sprend = GetComponent<SpriteRenderer> ();
+
+		SwapSprites();
+	}
+
+	void SwapSprites() {
+		if (isOn)
+			sprend.sprite = flashLightOn;
+		else
+			sprend.sprite = flashLightOff;
+
+		fieldOfViewLight.SetActive (isOn);
+	}
+
+	void Update() {
+		if ((isOn && sprend.sprite != flashLightOn) ||
+		    (!isOn && sprend.sprite != flashLightOff) ||
+		    (isOn && !fieldOfViewLight.activeSelf) ||
+		    (!isOn && fieldOfViewLight.activeSelf))
+		{
+			SwapSprites ();
+		}
+	}
+}
