393fffa4bb344ea006ae25db0da17b5aa29c45d4
Alex Turner <alexlt@umich.edu>
2017-02-26 14:03:47 -0500

    AI Patrols

--- Assets/Scripts/AI/Guard.cs
+++ Assets/Scripts/AI/Guard.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Guard : MonoBehaviour {
 
+	public enum GuardMode {
+		Patrol
+	}
+	public GuardMode mode;
+
+	public Transform[] patrolPoints;
+	public Transform aiPointsOfInterst;
+
+	private int destinationPoint;
+	private NavMeshAgent agent;
+
 	// Use this for initialization
 	void Start () {
-		
+		agent = GetComponent<NavMeshAgent> ();
+		agent.autoBraking = false;
+
+		GoToNextPoint ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (agent.remainingDistance < 0.5f)
+			GoToNextPoint ();
+	}
+
+	void GoToNextPoint() 
+	{
+		if (patrolPoints.Length == 0)
+			return;
+
+		agent.destination = patrolPoints [destinationPoint].position;
+
+		destinationPoint = (destinationPoint + 1) % patrolPoints.Length;
 	}
 }
