e7d321215c9d7a5ffde4055bbd963cc767bc1b4c
Michael Scheible <sheebs@umich.edu>
2017-03-05 04:08:21 -0500

    Saturday Work

---
+++ Assets/Scripts/Characters/MagicalNightstick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicalNightstick : Ability {
+    bool active = false;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    //reset position so it falls to the ground, just juice 
+    public override void UseAbility() {
+        Magician magician = GetComponentInParent<Magician>();
+        if (active) {
+            magician.using_ability = false;
+            this.gameObject.SetActive(false);
+        }
+        else {
+            magician.using_ability = true;
+        }
+        active = !active;
+    }
+//action function called when a player uses the magical nightstick
+    public void Action(GameObject mag) {
+        Magician magician = mag.GetComponent<Magician>();
+    }   
+
+}
---
+++ Assets/Scripts/Objects/Windows.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Windows : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Break() {
+       foreach (Transform t in transform) {
+            if (t.name == "Horizontal Cross Beam")
+                Destroy(t);
+            else if (t.name == "Vertical Cross Beam")
+                Destroy(t);
+        }
+    }
+}
