ff5f9e2d23d331802be998e1e1d2c02e085bdafe
Alex Turner <alexlt@umich.edu>
2017-03-12 20:21:45 -0400

    Backgrounds + Sunset preview

---
+++ Assets/Scripts/Sunset/SunsetTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunsetTimer : MonoBehaviour {
+
+	public static SunsetTimer instance;
+
+	public float alphaAmount = 1.0f;
+	public float transitionTime = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+		if (!instance)
+			instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		alphaAmount -= Time.deltaTime * 1.0f / transitionTime;
+
+		if (alphaAmount < 0)
+			alphaAmount = 0f;
+	}
+}
