0fe4f23e4d299dd8d4092221952aa29706d53fab
David Vader <davevad@umich.edu>
2017-04-07 03:23:03 -0400

    tweaked combat system, removed the obscene turd blunt dildo weapon SHEEEEEEEBS, added puff of smoke upon level entry and upon knnockout, changed forcefield to blue and flashes red when blocking attacks, change the way you attack a bit, holds knife out joust-style, same code but different animation. fixed spawn camping bugs. weapon now stops functioning on collision but does not immediately disappear (sprite), just juice.

---
+++ Assets/Scripts/SmokeLayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeLayer : MonoBehaviour {
+	float age;
+	float lifespan = 5;
+	float birth;
+	// Use this for initialization
+	void Start () {
+		GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingOrder = 50;
+		birth = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		age = Time.time - birth;
+		if (age > lifespan)
+			Destroy (this.gameObject);
+	}
+}
