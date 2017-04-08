4aa735f6cc9c5369b7bb5e57b74661a255c486b4
David Vader <davevad@umich.edu>
2017-04-08 13:46:58 -0400

    added lights juice, unsure if I like it

---
+++ Assets/StrobeLight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrobeLight : MonoBehaviour {
+	Light light;
+	public bool dim = true;
+	public bool brighten = false;
+	public bool dimmed = true;
+	public bool brightened = false;
+	float intensity = 1.25f;
+	public bool loose;
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+		InvokeRepeating ("Transition", 1.2f, 1.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (loose)
+			intensity = 2.5f;
+		if (dim) {
+			light.intensity = Mathf.Lerp (light.intensity, 0f, 0.1f);
+			if (light.intensity <= 0.1f) {
+				dim = false;
+				brighten = true;
+			}
+		} 
+		if (brighten) {
+			light.intensity = Mathf.Lerp (light.intensity, intensity, 0.1f);
+			if (light.intensity >= intensity - 0.1f) {
+				brighten = false;
+				dim = true;
+			}
+		}
+	}
+	void Transition(){
+	}
+}
