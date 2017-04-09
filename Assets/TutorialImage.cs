9128440009eccb632aa047ab57aa12ac2cb1e2b0
David Vader <davevad@umich.edu>
2017-04-09 15:30:53 -0400

    added tutorial scene

---
+++ Assets/TutorialImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialImage : MonoBehaviour {
+
+	public Sprite[] images;
+	int i = 0;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SwitchImage", 0.0f, 0.7f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void SwitchImage(){
+		GetComponent<Image>().sprite = images [i];
+		i = (i + 1) % images.Length;
+	}
+}
