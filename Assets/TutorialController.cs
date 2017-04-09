acd0dd74c7c16f02b4a53d767f3b6d6d2f5ab87d
David Vader <davevad@umich.edu>
2017-04-09 15:30:53 -0400

    added tutorial scene

---
+++ Assets/TutorialController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour {
+	public GameObject[] tutorials;
+	public int index;
+	// Use this for initialization
+	void Start () {
+		SoundsController.instance.DisableLooping ();
+		SoundsController.instance.StopPlaying ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+
+			index = 1;
+			if (tutorials [index - 1].activeSelf) {
+				tutorials [index - 1].SetActive (false);
+				return;
+			}
+			Invoke ("StartTutorial", 1f);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			index = 2;
+			if (tutorials [index - 1].activeSelf) {
+				tutorials [index - 1].SetActive (false);
+				return;
+			}
+			Invoke ("StartTutorial", 1f);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			index = 3;
+			if (tutorials [index - 1].activeSelf) {
+				tutorials [index - 1].SetActive (false);
+				return;
+			}
+			Invoke ("StartTutorial", 1f);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha5)) {
+
+			index = 4;
+			if (tutorials [index - 1].activeSelf) {
+				tutorials [index - 1].SetActive (false);
+				return;
+			}
+			Invoke ("StartTutorial", 1f);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha6)) {
+			index = 5;
+			if (tutorials [index - 1].activeSelf) {
+				tutorials [index - 1].SetActive (false);
+				return;
+			}
+			Invoke ("StartTutorial", 1f);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha7)) {
+			index = 6;
+			if (tutorials [index - 1].activeSelf) {
+				tutorials [index - 1].SetActive (false);
+				return;
+			}
+			Invoke ("StartTutorial", 1f);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha8)) {
+
+			index = 7;
+			if (tutorials [index - 1].activeSelf) {
+				tutorials [index - 1].SetActive (false);
+				return;
+			}
+			Invoke ("StartTutorial", 1f);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha9)) {
+			index = 8;
+			if (tutorials [index - 1].activeSelf) {
+				tutorials [index - 1].SetActive (false);
+				return;
+			}
+			Invoke ("StartTutorial", 1f);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha0)) {
+			index = 9;
+			if (tutorials [index - 1].activeSelf) {
+				tutorials [index - 1].SetActive (false);
+				return;
+			}
+			Invoke ("StartTutorial", 1f);
+		}
+	}
+	void StartTutorial(){
+		tutorials [index - 1].SetActive (true);
+		tutorials [index - 1].GetComponentInChildren<TutorialWrite> ().Write ();
+	}
+}
---
+++ Assets/TutorialWrite.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialWrite : MonoBehaviour {
+	float write_speed = 0.15f;
+	public bool write;
+	public string input;
+	string[] input_tokens;
+	int index = 0;
+	Text txt;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void Write(){
+		write = true;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (!txt)
+			txt = GetComponent<Text> ();
+
+		if (write) {
+			write = false;
+			txt.text = "";
+			TokenizeInput ();
+			WriteWord ();
+		}
+		
+	}
+	void WriteWord(){
+		if (index == input_tokens.Length) {
+			return;
+		}
+		txt.text += input_tokens [index++] + " "; 
+		Invoke ("WriteWord", write_speed);
+	}
+	void TokenizeInput(){
+		char[] delimit = { ' ' };
+		input_tokens = input.Split(delimit);
+	}
+}
