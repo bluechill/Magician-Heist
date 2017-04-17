6c45ca9ab9d40a278d6ae91ae72d67f9658375b3
Alex Turner <alexlt@umich.edu>
2017-04-17 11:52:09 -0400

    countdown sound + sunset

--- Assets/Scripts/Sunset/SunsetObject.cs
+++ Assets/Scripts/Sunset/SunsetObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SunsetObject : MonoBehaviour {
 
 	public SpriteRenderer sr;
+	public AnimationCurve alphaCurve;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,17 @@ public class SunsetObject : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		sr.color = new Color (1.0f, 1.0f, 1.0f, SunsetTimer.instance.alphaAmount);
+		float alpha = (Game.GameInstance.run_time / (Game.GameInstance.time_limit * 0.9f));
+
+		if (alpha < 0.0f)
+			alpha = 0.0f;
+		else if (alpha > 1.0f)
+			alpha = 1.0f;
+
+		alpha = 1.0f - alpha;
+
+		alpha = alphaCurve.Evaluate (alpha);
+
+		sr.color = new Color (1.0f, 1.0f, 1.0f, alpha);
 	}
 }
